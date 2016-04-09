8a212aea81b7d50792e8849be0548938b73c4b1c
Wenley Tong <wenley@squareup.com>
2016-04-08 18:04:22 -0700

    Flesh out building class and constructing graphs.

--- building.py
+++ building.py
@@ -1,35 +1,57 @@
 
-from collections import defaultdict
+from resource_flow import ResourceFlow
 
 class FactoryNode(object):
     '''
     Abstract class for objects that can be included in a resource flow graph.
     '''
     def inputs(self):
+        '''
+        @return ResourceFlow
+        '''
         pass
 
     def throughput(self):
+        '''
+        @return ResourceFlow
+        '''
         pass
 
     def __or__(self, other):
         '''
         Joins two Factory Nodes in parallel
-        '''
-        return OrNode(buildings=[self, other])
 
-    def __add__(self, next_node):
+        @return FactoryNode
         '''
-        Joins two Factory Nodes in series
+        return self + other
+
+    def __add__(self, other):
         '''
-        pass
+        Joins two Factory Nodes in parallel
+
+        @return FactoryNode
+        '''
+        return ParallelNode(buildings=[self, other])
+
+    def __lt__(self, other):
+        '''
+        Given a < b, chain the result of b into a
+        '''
+        return SeriesNode(other, self)
+
+    def __gt__(self, other):
+        '''
+        Given a > b, chain the result of a into b
+        '''
+        return SeriesNode(self, other)
 
 # Decorator that can be applied to a Building
 # Modifies its power consumption, speed, output, and pollution
 class Module(object):
     pass
 
-class Building(object):
-    def __init__(self, level, recipe, modules=[]):
+class Building(FactoryNode):
+    def __init__(self, recipe, modules=[]):
         self.recipe = recipe
         self.modules = modules
 
@@ -38,28 +60,27 @@ class Building(object):
         return self
 
     def inputs(self):
-        return reduce(lambda result, module: module.inputs(result), self.modules)
+        base = ResourceFlow.from_dict(self.recipe.inputs)
+        print "Base inputs:", base
+        return reduce(lambda result, module: module.inputs(result), self.modules, base)
 
     def throughput(self):
-        base = float(self.recipe.amount) / self.recipe.ticks
-        return reduce(lambda result, module: module.throughput(result), self.modules)
+        base_velocity = float(self.recipe.amount) / self.recipe.ticks
+        base = ResourceFlow.from_dict({ self.recipe.name: base_velocity })
 
-class OrNode(object):
+        return reduce(lambda result, module: module.throughput(result), self.modules, base)
+
+class ParallelNode(FactoryNode):
     def __init__(self, buildings=[]):
-        self.buildings = []
+        self.buildings = buildings
 
     def inputs(self):
-        result = defaultdict(lambda: 0.0)
-        for b in buildings:
-            ticks = b.ticks()
-            for item, amount in b.inputs.iteritems():
-                result[item] += amount
-        return result
+        return reduce(lambda a, b: a + b, (b.inputs() for b in self.buildings), ResourceFlow())
 
     def throughput(self):
-        return sum(b.throughput() for b in buildings())
+        return reduce(lambda a, b: a + b, (b.throughput() for b in self.buildings), ResourceFlow())
 
-class AndNode(object):
+class SeriesNode(FactoryNode):
     def __init__(self, producer, consumer):
         '''
         @param producer : FactoryNode - the node that feeds its output to consumer
@@ -69,14 +90,29 @@ class AndNode(object):
         self.consumer = consumer
 
     def inputs(self):
-        result = self.consumer.inputs()
-        result[producer.recipe.name] -= producer.throughput()
-
-        for item, amount in producer.inputs():
-            result[item] += amount
-        return result
+        return self.consumer.inputs() - self.producer.throughput() + self.producer.inputs()
 
     def throughput(self):
-        result = self.producer.throughput()
+        result = self.consumer.throughput()
+
+if __name__ == "__main__":
+    from recipe import recipe_for_item
+
+    gear = Building(recipe_for_item('gear'))
+    print 'Gear:'
+    print gear.inputs()
+    print gear.throughput()
+
+    double_gear = gear + gear
+    print "\nDouble Gear:"
+    print double_gear.inputs()
+    print double_gear.throughput()
+
+    transporter = Building(recipe_for_item('transporter'))
+    full_transporter = gear > transporter
+
+    print "\nTransporter:"
+    print full_transporter.inputs()
+    print full_transporter.throughput()
 
 
